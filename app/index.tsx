3519f8e6c2b9a4ac3ef374b0d33ec6e054e850f2
Your Name <you@example.com>
2024-09-27 15:04:02 +0200

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-blue">Welcome to the Marketplace application</Text>
+    </View>
+  );
+}
