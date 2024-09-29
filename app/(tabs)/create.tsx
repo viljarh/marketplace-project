7f9ca0ff540f626e53fa0dfd5301d9e1674bcbaa
Your Name <you@example.com>
2024-09-29 20:03:05 +0200

    some small changes on ui

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -2,13 +2,19 @@ import { SafeAreaView, ScrollView, Text, View } from "react-native";
 
 export default function CreateScreen() {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1">
       <View className="flex-row justify-center items-center px-4 mb-4 border-b border-gray-300 py-2">
         <Text className="text-lg font-semibold">Create Post</Text>
       </View>
 
-      <ScrollView className="flex-1 px-4 py-6">
+      <ScrollView
+        className="flex-1"
+        contentContainerStyle={{ paddingHorizontal: 16 }}
+      >
         <Text className="text-lg font-semibold">Title</Text>
+        <Text className="text-lg font-semibold">Description</Text>
+        <Text className="text-lg font-semibold">Price</Text>
+        <Text className="text-lg font-semibold">Condition</Text>
       </ScrollView>
     </SafeAreaView>
   );
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,12 @@
-import { router } from "expo-router";
 import {
   Text,
   View,
   SafeAreaView,
   TouchableOpacity,
   ScrollView,
-  TouchableOpacityBase,
 } from "react-native";
 import {
   ArrowRightEndOnRectangleIcon,
-  ChevronLeftIcon,
   Cog6ToothIcon,
   DocumentIcon,
   HeartIcon,
@@ -18,36 +15,40 @@ import {
 
 export default function ProfileScreen() {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1">
       <View className="flex-row justify-center items-center px-4 mb-4 border-b border-gray-300 py-2">
         <Text className="text-lg font-semibold">Profile</Text>
       </View>
 
-      <View className="flex items-center my-6">
-        <UserCircleIcon size={64} color="gray" />
-        <Text className="text-lg font-semibold mt-2">Viljar Hoem-Olsen</Text>
-        <Text className="text-gray-500">viljar.hoem.olsen@gmail.com </Text>
-      </View>
+      <ScrollView className="flex-1">
+        <View className="flex items-center my-6">
+          <UserCircleIcon size={64} color="gray" />
+          <Text className="text-lg font-semibold mt-2">Viljar Hoem-Olsen</Text>
+          <Text className="text-gray-500">viljar.hoem.olsen@gmail.com </Text>
+        </View>
+
+        <View className="p-4">
+          <TouchableOpacity className="flex-row justify-between items-center py-4 border-t border-gray-200">
+            <Text className="text-base">My Posts</Text>
+            <DocumentIcon size={24} color="gray" />
+          </TouchableOpacity>
+
+          <TouchableOpacity className="flex-row justify-between items-center py-4 border-t border-gray-200">
+            <Text className="text-base">My Favorites</Text>
+            <HeartIcon size={24} color="gray" />
+          </TouchableOpacity>
+
+          <TouchableOpacity className="flex-row justify-between items-center py-4 border-t border-gray-200">
+            <Text className="text-base">Settings</Text>
+            <Cog6ToothIcon size={24} color="gray" />
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
 
       <View className="p-4">
         <TouchableOpacity className="flex-row justify-between items-center py-4 border-t border-gray-200">
-          <Text className="text-base">My Posts</Text>
-          <DocumentIcon size={24} color="gray" />
-        </TouchableOpacity>
-
-        <TouchableOpacity className="flex-row justify-between items-center py-4 border-t border-gray-200">
-          <Text className="text-base">My Favorites</Text>
-          <HeartIcon size={24} color="gray" />
-        </TouchableOpacity>
-
-        <TouchableOpacity className="flex-row justify-between items-center py-4 border-t border-gray-200">
-          <Text className="text-base">Settings</Text>
-          <Cog6ToothIcon size={24} color="gray" />
-        </TouchableOpacity>
-
-        <TouchableOpacity className="flex-row justify-between items-center py-4 border-t border-gray-200">
-          <Text className="text-base">Log Out</Text>
-          <ArrowRightEndOnRectangleIcon size={24} color="gray" />
+          <Text className="text-red-600 font-semibold">Log Out</Text>
+          <ArrowRightEndOnRectangleIcon size={24} color="red" />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
