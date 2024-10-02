6cf1d6413ed5d1615b617bf0a2c8d9e6f83c5580
Your Name <you@example.com>
2024-10-02 09:35:41 +0200

    fixed error from layout where routes were not found

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,6 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="feed" options={{ headerShown: false }} />
-      <Stack.Screen name="settings" options={{ headerShown: false }} />
     </Stack>
   );
 }
