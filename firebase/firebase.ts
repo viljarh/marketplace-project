9c00c3e3ac3508455106996dc8543070bf697db8
Your Name <you@example.com>
2024-10-02 12:13:23 +0200

    initialized firebase into the project

---
+++ firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD91ADessOQ__nBZTU2-zvp58gf0jwEUTM",
+  authDomain: "marketplace-65a54.firebaseapp.com",
+  projectId: "marketplace-65a54",
+  storageBucket: "marketplace-65a54.appspot.com",
+  messagingSenderId: "40513142793",
+  appId: "1:40513142793:web:74d0abbdb0182cfcb9b8d8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
