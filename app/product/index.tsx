34e036e29f43a5a2263522ab18ea09ccee165ca0
Eirik <eirik.imrik@gmail.com>
2024-10-02 12:22:58 +0200

    added image placeholder and started on contents

--- app/product/index.tsx
+++ app/product/index.tsx
@@ -4,6 +4,7 @@ import {
   Text,
   TouchableOpacity,
   StyleSheet,
+  Image,
 } from "react-native";
 import { ChevronLeftIcon } from "react-native-heroicons/outline";
 import { router } from "expo-router";
@@ -11,8 +12,8 @@ import { router } from "expo-router";
 export default function ProductDetails() {
   return (
     <SafeAreaView>
-      <View className="flex-row justify-between items-center px-4 mb-4 border-b border-gray-300 py-2">
-      <TouchableOpacity
+      <View className="flex-row justify-between items-center px-4 border-b border-gray-300 py-2">
+        <TouchableOpacity
           className="flex-row items-center"
           onPress={() => router.back()}
         >
@@ -20,12 +21,16 @@ export default function ProductDetails() {
           <Text className="text-gray-500 ml-1">Back</Text>
         </TouchableOpacity>
         <Text className="text-lg font-semibold">Product details</Text>
-        <View style={{ width: 50}}/>
-        
+        <View style={{ width: 50 }} />
       </View>
       <View style={styles.container}>
-        
-        <Text>Product Details</Text>
+        <Image
+          style={styles.image}
+          source={require("../../assets/images/placeholder.png")}
+        />
+        <View style={styles.contentContainer}>
+          <Text style={{ fontWeight: 600, fontSize: 18 }}>Product Name</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -33,7 +38,20 @@ export default function ProductDetails() {
 
 const styles = StyleSheet.create({
   container: {
+    height: "100%",
     flexDirection: "column",
-    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+  },
+  image: {
+    width: "100%",
+    height: 300,
+    backgroundColor: "#D9D9D9",
+  },
+  contentContainer: {
+    width: "100%",
+    height: "100%",
+    padding: 20,
+    backgroundColor: "white",
   },
 });
