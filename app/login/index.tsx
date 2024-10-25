0555d398dcf804ee4539d99b4c2dd204a7d29040
Your Name <you@example.com>
2024-10-25 10:18:04 +0200

    reset

---
+++ app/login/index.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function LoginPage() {
+  return (
+    <SafeAreaView>
+      <Text>Login Screen</Text>
+    </SafeAreaView>
+  );
+}
