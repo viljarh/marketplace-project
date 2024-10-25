1b9fa52e5e85eaea32eac42e0c017267f4aceadc
Your Name <you@example.com>
2024-10-25 11:25:30 +0200

    added functions to firebase file

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,6 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  User,
+  signOut as firebaseSignOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,3 +27,61 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const auth = getAuth(app);
+
+export { auth };
+
+export const signUp = async (
+  email: string,
+  password: string
+): Promise<User | null> => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log("User successfully created", userCredential.user);
+    return userCredential.user;
+  } catch (error: any) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const signIn = async (
+  email: string,
+  password: string
+): Promise<User | null> => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log("User successfully signed in", userCredential.user);
+    return userCredential.user;
+  } catch (error: any) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const signOut = async (): Promise<void> => {
+  try {
+    await firebaseSignOut(auth);
+    console.log("User successfully signed out");
+  } catch (error: any) {
+    console.error("Error signing out", error.message);
+    throw error;
+  }
+};
+
+export const getCurrentUser = (): User | null => {
+  return auth.currentUser;
+};
+
+export const onAuthStateChangeListener = (
+  callback: (user: User | null) => void
+) => {
+  return onAuthStateChanged(auth, callback);
+};
