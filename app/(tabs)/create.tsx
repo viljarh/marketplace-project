62ad3f8a7dc3ef7a8c5a5b80387c91df8e06a9cd
Jerezard <jeremys-1999@hotmail.com>
2024-11-08 03:20:20 +0100

    Create product now sends data to firestore

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -8,8 +8,45 @@ import {
 } from "react-native";
 import { CameraIcon, ChevronLeftIcon } from "react-native-heroicons/outline";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { db } from '../../firebase/firebase';
+import { useState } from "react";
+import{ useRouter } from 'expo-router';
+import { addDoc, collection, Timestamp } from "firebase/firestore";
 
 export default function CreatePostScreen() {
+
+ 
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [category, setCategory] = useState('');
+    const [condition, setCondition] = useState('');
+    
+    const router = useRouter();
+
+    
+    const handleCreatePost = async () => {
+     
+      const productData = {
+        title,
+        description,
+        price,
+        category,
+        condition,
+        createdAt: Timestamp.now(), 
+      };
+      
+      try {
+
+        const docRef = await addDoc(collection(db, 'product'), productData);
+        console.log('Product created successfully with ID:', docRef.id)
+
+        router.back(); 
+      } catch (error) {
+        console.error('Error creating product:', error);
+      }
+    };
+
   return (
     <SafeAreaView className="flex-1">
       <View className="flex-row justify-between items-center px-4 mb-4 border-b border-gray-300 py-2">
@@ -34,12 +71,16 @@ export default function CreatePostScreen() {
         <TextInput
           className="border border-gray-300 rounded-lg p-4 mb-4"
           placeholder="Enter title"
+          value={title}
+          onChangeText={setTitle}
         />
 
         <Text className="text-lg font-semibold mb-2">Description</Text>
         <TextInput
           className="border border-gray-300 rounded-lg p-4 mb-4"
           placeholder="Enter description"
+          value={description}
+          onChangeText={setDescription}
           multiline={true}
         />
 
@@ -48,18 +89,24 @@ export default function CreatePostScreen() {
           className="border border-gray-300 rounded-lg p-4 mb-4"
           placeholder="Enter price"
           keyboardType="numeric"
+          value = {price}
+          onChangeText = {setPrice}
         />
 
         <Text className="text-lg font-semibold mb-2">Select Category</Text>
         <TextInput
           className="border border-gray-300 rounded-lg p-4 mb-4"
           placeholder="Choose category"
+          value={category}
+          onChangeText={setCategory}
         />
 
         <Text className="text-lg font-semibold mb-2">Condition</Text>
         <TextInput
           className="border border-gray-300 rounded-lg p-4 mb-4"
           placeholder="Choose condition"
+          value={condition}
+          onChangeText={setCondition}
         />
 
         <Text className="text-lg font-semibold mb-2">Images</Text>
@@ -69,7 +116,7 @@ export default function CreatePostScreen() {
         </TouchableOpacity>
 
         <View className="pb-4">
-          <TouchableOpacity className="bg-blue-500 rounded-lg p-4">
+          <TouchableOpacity onPress = {handleCreatePost} className="bg-blue-500 rounded-lg p-4">
             <Text className="text-white text-center font-semibold  text-lg">
               Create Post
             </Text>
